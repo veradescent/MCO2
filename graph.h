9fa1e11b5714023fde176c0768121ba1adc72bdd
veradescent <148369761+veradescent@users.noreply.github.com>
2024-07-21 14:35:15 +0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,20 +1,23 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#define MAX_SIZE 100
+#define MAX_VERTICES 100
+#define MAX_STRING_LENGTH 100
 
 typedef struct {
-    int vertex;
+    char vertex[MAX_STRING_LENGTH];
     Node* next;
 } Node;
 
 typedef struct {
     int numVertices;
     Node** adjLists;
+    char** vertices;
 } Graph;
 
-void addEdges();
-void addNode();
-void createGraph();
+Graph* createGraph(int vertices);
+Node* createNode(const char* vertex);
+int getVertexIndex(Graph* graph, const char* vertex);
+void addEdge(Graph* graph, const char* src, const char* dest);
 
 #endif
